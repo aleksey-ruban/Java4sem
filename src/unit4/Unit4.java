a3459c3eab0ad97a843fef95d8ac4dec84283761
aleksey-ruban <alekseyruban555@gmail.com>
2024-02-26 16:01:59 +0300

    Unit4

---
+++ src/unit4/Unit4.java
@@ -0,0 +1,50 @@
+package unit4;
+
+import java.util.concurrent.Future;
+
+public class Unit4 {
+
+    public static void main(String args[]) {
+        MyExecutorService exService = new MyExecutorService(2);
+
+        Future f = exService.submit(() -> {
+            try {
+                Thread.sleep(20000);
+                System.out.println("20 seconds sleep");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        exService.submit(() -> {
+            try {
+                Thread.sleep(4000);
+                System.out.println("4 seconds sleep");
+                f.cancel(true);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        exService.submit(() -> {
+            try {
+                Thread.sleep(3000);
+                System.out.println("3 seconds sleep");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        exService.submit(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("2 seconds sleep");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        exService.shutdown();
+
+    }
+}
